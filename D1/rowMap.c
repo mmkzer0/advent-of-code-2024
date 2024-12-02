0d0334e7421b72d66662dff3276722aa45d59e5c
Reaven <len.kot@icloud.com>
2024-12-02 10:59:17 +0100

    Day 1 solution using C

---
+++ D1/rowMap.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINES 10000
+#define MAX_LEN 1024
+
+int compare_integers(const void *a, const void *b) {
+  const int *int1 = (const int *)a;
+  const int *int2 = (const int *)b;
+  return (*int1 > *int2) -
+         (*int1 < *int2); // Alternative to `return (*int1 - *int2)`
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
+    return 1;
+  }
+
+  FILE *file = fopen(argv[1], "r");
+  if (file == NULL) {
+    perror("Error opening file!\n");
+    return 1;
+  }
+
+  char line[MAX_LEN];
+  int firstTokens[MAX_LINES];
+  int secTokens[MAX_LINES];
+  int linecount = 0;
+
+  while (fgets(line, sizeof(line), file)) {
+    if (linecount >= MAX_LINES) {
+      fprintf(stderr, "Error: too may lines!\n");
+      break;
+    }
+
+    // trim newline
+    line[strcspn(line, "\n")] = '\0';
+
+    char *tok = strtok(line, " \t");
+    firstTokens[linecount] = atoi(tok);
+
+    tok = strtok(NULL, " \t");
+    secTokens[linecount] = atoi(tok);
+
+    linecount++;
+  }
+
+  fclose(file);
+
+  qsort(firstTokens, linecount, sizeof(int), compare_integers);
+  qsort(secTokens, linecount, sizeof(int), compare_integers);
+
+  int out = 0;
+  for (int i = 0; i < linecount; i++) {
+    int a = firstTokens[i];
+    int b = secTokens[i];
+
+    if (a > b) {
+      out += a - b;
+    } else {
+      out += b - a;
+    }
+  }
+
+  printf("Result: %d\n", out);
+
+  return 0;
+}
